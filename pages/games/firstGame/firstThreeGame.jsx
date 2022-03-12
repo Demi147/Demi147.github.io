2a167eef781f14a37cae5a29beba71cc3e6bb57f
Carel Haasbroek <productionsDemigod@gmail.com>
2022-03-12 10:19:39 +0200

    HMMM

--- pages/games/firstGame/firstThreeGame.jsx
+++ pages/games/firstGame/firstThreeGame.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import * as THREE from "three";
-import { generateCar } from "../../lib/firstGameLib/helperMethods";
+import { generateCar } from "../../../shared/lib/helperMethods";
 
 class FirstGame extends Component {
   componentWillUnmount() {
